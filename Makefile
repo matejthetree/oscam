matejthetree/oscam

commit d16d1e074a9796be65a34159706f1dac8ca07428

Fix some edge cases
- SSL: support OpenSSL installations in non standard locations (cmake)
- SSL: show OpenSSL include directory in buildlog (cmake)
- SSL: remove -Wunused-value warning for SSL_CTX_set_ecdh_auto dispayed on some systems
- SIGNING: ensure a missing signing certificate is created on first build (cmake)
- SIGNING: fix filtering oscam-signing.[c|h] when WITH_SIGNING is disabled (cmake)
- LIBDVBCSA: fix link filename (cmake)
- LIBDL: link libdl by default for Linux as it is required by some systems (cmake)
- CONFIG: Fix display of configured values

CMakeLists.txt
@@ -223,21 +223,23 @@ if (CONFIG_WITH_SIGNING MATCHES "N" AND WITH_SIGNING EQUAL 1)
 	message (STATUS "SIGNING is added by You compiling with signing included")
 elseif (CONFIG_WITH_SIGNING MATCHES "Y" AND WITH_SIGNING EQUAL 1)
 	message (STATUS "SIGNING is added by You but it was already enabled by config file")
-elseif (CONFIG_WITH_SIGNING MATCHES "N" AND NOT WITH_SIGNING EQUAL 1)
-	message (STATUS "SIGNING IS NOT INCLUDED IN THIS COMPILATION")
+elseif (CONFIG_WITH_SIGNING MATCHES "Y" AND NOT DEFINED WITH_SIGNING)
+	message(STATUS "SIGNING is added by config file ${CONFIG_WITH_SIGNING} compiling with signing included")
+elseif (CONFIG_WITH_SIGNING MATCHES "N" OR WITH_SIGNING EQUAL 0)
+	set (WITH_SIGNING "0")
+	message (STATUS "SIGNING is disabled")
 endif (CONFIG_WITH_SIGNING MATCHES "N" AND WITH_SIGNING EQUAL 1)
 
-if (CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0)
+if ((CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0) OR WITH_SIGNING EQUAL 1)
 	execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --create-cert ecdsa prime256v1 ca ERROR_QUIET)
 	execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-file cert RESULT_VARIABLE CONFIG_RET ERROR_QUIET OUTPUT_VARIABLE SIGN_CERT OUTPUT_STRIP_TRAILING_WHITESPACE)
 	if (NOT CONFIG_RET EQUAL 0)
 		set (WITH_SIGNING "0")
 		message (STATUS "SIGNING requested but certificates NOT FOUND  COMPILING WHITOUT SIGNING !!!")
 	else (NOT CONFIG_RET EQUAL 0)
-		message(STATUS "SIGNING is added by config file ${CONFIG_WITH_SIGNING} compiling with signing included")
 		add_definitions ("-DWITH_SIGNING -DWITH_SSL")
 		set (WITH_SIGNING "1")
-		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-file privkey                             OUTPUT_VARIABLE SIGN_PRIVKEY OUTPUT_STRIP_TRAILING_WHITESPACE)
+		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-file privkey ERROR_QUIET                 OUTPUT_VARIABLE SIGN_PRIVKEY OUTPUT_STRIP_TRAILING_WHITESPACE)
 		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --sign-marker                                   OUTPUT_VARIABLE SIGN_MARKER OUTPUT_STRIP_TRAILING_WHITESPACE)
 		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-info COMMAND head -n 1                   OUTPUT_VARIABLE SIGN_SUBJECT OUTPUT_STRIP_TRAILING_WHITESPACE)
 		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-info COMMAND tail -n 1                   OUTPUT_VARIABLE SIGN_SIGALGO OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -251,7 +253,7 @@ if (CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0)
 
 elseif (CONFIG_WITH_SIGNING MATCHES "Y" AND WITH_SIGNING EQUAL 0)
 	message (STATUS  " The config file has signing enabled, but You disabled it by you cmake command COMPILING WHITOUT SIGNING")
-endif (CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0)
+endif ((CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0) OR WITH_SIGNING EQUAL 1)
 
 #----------------------- subdirectories ------------------------------
 
@@ -283,7 +285,7 @@ elseif (CONFIG_WITH_SSL MATCHES "N" AND NOT WITH_SSL EQUAL 1)
 endif (CONFIG_WITH_SSL MATCHES "N" AND WITH_SSL EQUAL 1)
 
 if (CONFIG_WITH_SSL MATCHES "Y" AND NOT WITH_SSL EQUAL 0)
-	message(STATUS "  SSL is added by config file ${CONFIG_WITH_SSL} compiling with ssl included")
+	message(STATUS " ssl is added by config file ${CONFIG_WITH_SSL} compiling with ssl included")
 	add_definitions ("-DWITH_SSL")
 	set (WITH_SSL "1")
 elseif (CONFIG_WITH_SSL MATCHES "Y" AND WITH_SSL EQUAL 0)
@@ -293,7 +295,7 @@ endif (CONFIG_WITH_SSL MATCHES "Y" AND NOT WITH_SSL EQUAL 0)
 find_package(OpenSSL)
 if (OPENSSL_FOUND AND HAVE_LIBCRYPTO)
 	include_directories(${OPENSSL_INCLUDE_DIR})
-	message(STATUS "  openssl found.")
+	message(STATUS "  OpenSSL found. Using header files located at ${OPENSSL_INCLUDE_DIR}.")
 	if (WITH_SSL EQUAL 1)
 		set(USE_SSL "USE_SSL")
 	endif (WITH_SSL EQUAL 1)
@@ -494,7 +496,7 @@ if (CONFIG_STREAMRELAY MATCHES "Y" AND NOT MODULE_STREAMRELAY EQUAL 1)
 			message(STATUS "  libdvbcsa found (libdvbcsa.so).")
 			add_library(imp_libdvbcsa SHARED IMPORTED)
 			set_property(TARGET imp_libdvbcsa PROPERTY IMPORTED_LOCATION ${LIBDVBCSA_LIBRARY} )
-			set(dvbcsa_link dvbcsa)
+			set(dvbcsa_link "imp_libdvbcsa")
 			set (STATICLIBDVBCSA 0)
 		else (STATIC_LIBDVBCSA AND FOUND_LIBDVBCSA AND EXISTS ${LIBADVBCSA_LIBRARY})
 			message(FATAL_ERROR "  no libdvbcsa found!")
@@ -580,9 +582,10 @@ add_library (${csreaders} STATIC ${csreaders_srcs} ${csreaders_hdrs})
 #----------------------- other oscam files ------------------------------
 
 set (csoscam "csoscam")
-if (NOT WITH_SIGNING EQUAL 1)
-	list(FILTER csoscam_srcs EXCLUDE REGEX ".*oscam-signing\\..*$")
-endif (NOT WITH_SIGNING EQUAL 1)
+if (NOT WITH_SIGNING)
+	list(REMOVE_ITEM csoscam_srcs "${CMAKE_SOURCE_DIR}/oscam-signing.c")
+	list(REMOVE_ITEM csoscam_hdrs "${CMAKE_SOURCE_DIR}/oscam-signing.h")
+endif (NOT WITH_SIGNING)
 add_library (${csoscam} STATIC ${csoscam_srcs} ${csoscam_hdrs})
 
 #----------------------- the executable ------------------------------
@@ -653,11 +656,11 @@ if (HAVE_PTHREAD)
 endif (HAVE_PTHREAD)
 
 if (WITH_SSL)
-	target_link_libraries (${exe_name} ssl)
+	target_link_libraries (${exe_name} "${OPENSSL_SSL_LIBRARIES}")
 endif (WITH_SSL)
 
 if (HAVE_LIBCRYPTO)
-	target_link_libraries (${exe_name} crypto)
+	target_link_libraries (${exe_name} "${OPENSSL_CRYPTO_LIBRARIES}")
 endif (HAVE_LIBCRYPTO)
 
 # some systems need libm for the nagra cak7 functions to work
@@ -691,6 +694,7 @@ if (NOT OSCamOperatingSystem MATCHES "(Mac OS X|Windows/Cygwin|FreeBSD|OpenBSD)"
 endif (NOT OSCamOperatingSystem MATCHES "(Mac OS X|Windows/Cygwin|FreeBSD|OpenBSD)")
 
 if (OSCamOperatingSystem MATCHES "Linux")
+	set (dl_link "dl")
 elseif (OSCamOperatingSystem MATCHES "Mac OS X")
 	if (NOT HAVE_PCSC EQUAL 0)
 		FIND_LIBRARY (PCSC_LIBRARY PCSC)

config.sh
@@ -440,7 +440,7 @@ make_config_mak() {
 }
 
 check_test() {
-	if [ "$(cat $tempfileconfig | grep "^#define $1 1$")" != "" ]; then
+	if [ "$(cat $configfile | grep "^#define $1 1$")" != "" ]; then
 		echo "on"
 	else
 		echo "off"
@@ -488,34 +488,32 @@ print_components() {
 	for i in $card_readers; do
 		printf "\t%-20s: %s\n" $i $(check_test "$i")
 	done
-
-	cp -f $tempfileconfig $configfile
 }
 
 menu_addons() {
 	${DIALOG} --checklist "\nChoose add-ons:\n " $height $width $listheight \
-		WEBIF				"Web Interface"							$(check_test "WEBIF") \
-		WEBIF_LIVELOG		"LiveLog"								$(check_test "WEBIF_LIVELOG") \
-		WEBIF_JQUERY		"Jquery onboard (if disabled webload)"	$(check_test "WEBIF_JQUERY") \
-		WITH_COMPRESS_WEBIF	"Compress webpages"						$(check_test "WITH_COMPRESS_WEBIF") \
-		WITH_SSL			"OpenSSL support"						$(check_test "WITH_SSL") \
-		HAVE_DVBAPI			"DVB API"								$(check_test "HAVE_DVBAPI") \
-		WITH_EXTENDED_CW	"DVB API EXTENDED CW API"				$(check_test "WITH_EXTENDED_CW") \
-		WITH_NEUTRINO		"Neutrino support"						$(check_test "WITH_NEUTRINO") \
-		READ_SDT_CHARSETS	"DVB API read-sdt charsets"				$(check_test "READ_SDT_CHARSETS") \
-		CS_ANTICASC			"Anti cascading"						$(check_test "CS_ANTICASC") \
-		WITH_DEBUG			"Debug messages"						$(check_test "WITH_DEBUG") \
-		MODULE_MONITOR		"Monitor"								$(check_test "MODULE_MONITOR") \
-		WITH_LB				"Loadbalancing"							$(check_test "WITH_LB") \
-		CS_CACHEEX			"Cache exchange"						$(check_test "CS_CACHEEX") \
-		CS_CACHEEX_AIO		"Cache exchange aio (depend on Cache exchange)"			$(check_test "CS_CACHEEX_AIO") \
-		CW_CYCLE_CHECK		"CW Cycle Check"						$(check_test "CW_CYCLE_CHECK") \
-		LCDSUPPORT			"LCD support"							$(check_test "LCDSUPPORT") \
-		LEDSUPPORT			"LED support"							$(check_test "LEDSUPPORT") \
-		CLOCKFIX			"Clockfix (disable on old systems!)"	$(check_test "CLOCKFIX") \
-		IPV6SUPPORT			"IPv6 support (experimental)"			$(check_test "IPV6SUPPORT") \
-		WITH_ARM_NEON		"ARM NEON (SIMD/MPE) support"			$(check_test "WITH_ARM_NEON") \
-		WITH_SIGNING		"Binary signing with X.509 certificate"	$(check_test "WITH_SIGNING") \
+		WEBIF				"Web Interface"									$(check_test "WEBIF") \
+		WEBIF_LIVELOG		"LiveLog"										$(check_test "WEBIF_LIVELOG") \
+		WEBIF_JQUERY		"Jquery onboard (if disabled webload)"			$(check_test "WEBIF_JQUERY") \
+		WITH_COMPRESS_WEBIF	"Compress webpages"								$(check_test "WITH_COMPRESS_WEBIF") \
+		WITH_SSL			"OpenSSL support"								$(check_test "WITH_SSL") \
+		HAVE_DVBAPI			"DVB API"										$(check_test "HAVE_DVBAPI") \
+		WITH_EXTENDED_CW	"DVB API EXTENDED CW API"						$(check_test "WITH_EXTENDED_CW") \
+		WITH_NEUTRINO		"Neutrino support"								$(check_test "WITH_NEUTRINO") \
+		READ_SDT_CHARSETS	"DVB API read-sdt charsets"						$(check_test "READ_SDT_CHARSETS") \
+		CS_ANTICASC			"Anti cascading"								$(check_test "CS_ANTICASC") \
+		WITH_DEBUG			"Debug messages"								$(check_test "WITH_DEBUG") \
+		MODULE_MONITOR		"Monitor"										$(check_test "MODULE_MONITOR") \
+		WITH_LB				"Loadbalancing"									$(check_test "WITH_LB") \
+		CS_CACHEEX			"Cache exchange"								$(check_test "CS_CACHEEX") \
+		CS_CACHEEX_AIO		"Cache exchange aio (depend on Cache exchange)"	$(check_test "CS_CACHEEX_AIO") \
+		CW_CYCLE_CHECK		"CW Cycle Check"								$(check_test "CW_CYCLE_CHECK") \
+		LCDSUPPORT			"LCD support"									$(check_test "LCDSUPPORT") \
+		LEDSUPPORT			"LED support"									$(check_test "LEDSUPPORT") \
+		CLOCKFIX			"Clockfix (disable on old systems!)"			$(check_test "CLOCKFIX") \
+		IPV6SUPPORT			"IPv6 support (experimental)"					$(check_test "IPV6SUPPORT") \
+		WITH_ARM_NEON		"ARM NEON (SIMD/MPE) support"					$(check_test "WITH_ARM_NEON") \
+		WITH_SIGNING		"Binary signing with X.509 certificate"			$(check_test "WITH_SIGNING") \
 		2> ${tempfile}
 
 	opt=${?}
@@ -632,9 +630,10 @@ config_dialog() {
 			Readers) menu_readers ;;
 			CardReaders) menu_card_readers ;;
 			Save)
-				print_components
+				cp -f $tempfileconfig $configfile
 				update_deps
 				write_enabled
+				print_components
 				exit 0
 			;;
 		esac

globals.h
@@ -372,7 +372,7 @@
 #define WIKI_URL				"https://wiki.streamboard.tv/wiki"
 #define BOARD_URL				"https://board.streamboard.tv"
 #ifndef CS_VERSION
-#define CS_VERSION				"2.24.08-11818"
+#define CS_VERSION				"2.24.08-11819"
 #endif
 #ifndef CS_GIT_COMMIT
 #define CS_GIT_COMMIT			"a2b4c6d8"

module-webif-lib.c
@@ -863,7 +863,7 @@ SSL_CTX *SSL_Webif_Init(void)
 	ctx = SSL_CTX_new(SSLv23_server_method());
 
 #if defined(SSL_CTX_set_ecdh_auto)
-		SSL_CTX_set_ecdh_auto(ctx, 1);
+		(void) SSL_CTX_set_ecdh_auto(ctx, 1);
 #elif defined(EC_PKEY_NO_PARAMETERS) && defined(NID_X9_62_prime256v1)
 		EC_KEY *ecdh = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
 		if(ecdh)

commit 4f908ce29481ba7dbdcbe735db4484c184da982d

signing: increase binary hash to sha256
- replace weaker sha1 hash with sha256
- include openssl version information for signing
- show openssl version for signing as signer in oscam.version
- fix config.sh keep selection error introduced in https://git.streamboard.tv/common/oscam/-/commit/d16d1e074a9796be65a34159706f1dac8ca07428

CMakeLists.txt
@@ -247,8 +247,8 @@ if ((CONFIG_WITH_SIGNING MATCHES "Y" AND NOT WITH_SIGNING EQUAL 0) OR WITH_SIGNI
 		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-info COMMAND head -n 5 COMMAND tail -n 1 OUTPUT_VARIABLE SIGN_PUBALGO OUTPUT_STRIP_TRAILING_WHITESPACE)
 		execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --cert-info COMMAND head -n 6 COMMAND tail -n 1 OUTPUT_VARIABLE SIGN_PUBBIT OUTPUT_STRIP_TRAILING_WHITESPACE)
 		set (SIGN_PUBKEY "${CMAKE_CURRENT_BINARY_DIR}/signing/pkey")
-		set (SIGN_HASH "${CMAKE_CURRENT_BINARY_DIR}/signing/sha1")
-		set (SIGN_DIGEST "${CMAKE_CURRENT_BINARY_DIR}/signing/sha256")
+		set (SIGN_HASH "${CMAKE_CURRENT_BINARY_DIR}/signing/sha256")
+		set (SIGN_DIGEST "${CMAKE_CURRENT_BINARY_DIR}/signing/digest")
 	endif (NOT CONFIG_RET EQUAL 0)
 
 elseif (CONFIG_WITH_SIGNING MATCHES "Y" AND WITH_SIGNING EQUAL 0)
@@ -602,10 +602,10 @@ if (USE_COMPRESS EQUAL 1)
 endif (USE_COMPRESS EQUAL 1)
 if (WITH_SIGNING EQUAL 1)
 	file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/signing)
-	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND sha1sum ${exe_name} | awk '{ print $$1 }' | tr -d '\\n' > ${SIGN_HASH})
+	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND sha256sum ${exe_name} | awk '{ print $$1 }' | tr -d '\\n' > ${SIGN_HASH})
 	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND openssl x509 -pubkey -noout -in ${SIGN_CERT}         -out ${SIGN_PUBKEY})
 	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND openssl dgst -sha256      -sign ${SIGN_PRIVKEY}      -out ${SIGN_DIGEST} ${SIGN_HASH})
-	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND printf '[SIGN] SHA1\('\; stat -c %s ${SIGN_HASH} | tr -d '\\n'\; printf '\): '\; cat ${SIGN_HASH}\; printf ' -> ')
+	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND printf '[SIGN] SHA256\('\; stat -c %s ${SIGN_HASH} | tr -d '\\n'\; printf '\): '\; cat ${SIGN_HASH}\; printf ' -> ')
 	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND openssl dgst -sha256    -verify ${SIGN_PUBKEY} -signature ${SIGN_DIGEST} ${SIGN_HASH} | tr -d '\\n')
 	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND printf '${SIGN_MARKER}' | cat - ${SIGN_DIGEST} >> ${exe_name})
 	add_custom_command(TARGET ${exe_name} POST_BUILD COMMAND printf ' <- DIGEST\('\; stat -c %s ${SIGN_DIGEST} | tr -d '\\n'\; printf '\)\\n')

Makefile
@@ -69,6 +69,7 @@ override STD_DEFS += -D'CS_CONFDIR="$(CONF_DIR)"'
 CC = $(CROSS_DIR)$(CROSS)gcc
 STRIP = $(CROSS_DIR)$(CROSS)strip
 UPX = $(shell which upx 2>/dev/null || true)
+SSL = $(shell which openssl 2>/dev/null || true)
 
 # Compiler warnings
 CC_WARN = -W -Wall -Wshadow -Wredundant-decls -Wstrict-prototypes -Wold-style-definition
@@ -122,19 +123,21 @@ ifeq "$(shell ./config.sh --enabled WITH_SIGNING)" "Y"
 		SIGN_PRIVKEY   = $(shell ./config.sh --cert-file privkey)
 		SIGN_MARKER    = $(shell ./config.sh --sign-marker)
 		SIGN_PUBKEY    = $(OBJDIR)/signing/pkey
-		SIGN_HASH      = $(OBJDIR)/signing/sha1
-		SIGN_DIGEST    = $(OBJDIR)/signing/sha256
+		SIGN_HASH      = $(OBJDIR)/signing/sha256
+		SIGN_DIGEST    = $(OBJDIR)/signing/digest
 		SIGN_SUBJECT   = $(shell ./config.sh --cert-info | head -n 1)
 		SIGN_SIGALGO   = $(shell ./config.sh --cert-info | tail -n 1)
 		SIGN_VALID     = $(shell ./config.sh --cert-info | head -n 4 | tail -n 1)
 		SIGN_PUBALGO   = $(shell ./config.sh --cert-info | head -n 5 | tail -n 1)
 		SIGN_PUBBIT    = $(shell ./config.sh --cert-info | head -n 6 | tail -n 1)
-		SIGN_INFO      = $(shell echo '|  Signing  : $(SIGN_PUBALGO), $(SIGN_PUBBIT), $(SIGN_SIGALGO),\n|             Valid $(SIGN_VALID), $(SIGN_SUBJECT)\n')
-		SIGN_COMMAND_OSCAM += sha1sum $@ | awk '{ print $$1 }' | tr -d '\n' > $(SIGN_HASH);
-		SIGN_COMMAND_OSCAM += printf 'SIGN	SHA1('; stat -c %s $(SIGN_HASH) | tr -d '\n'; printf '): '; cat $(SIGN_HASH); printf ' -> ';
-		SIGN_COMMAND_OSCAM += openssl x509 -pubkey -noout -in $(SIGN_CERT)         -out $(SIGN_PUBKEY);
-		SIGN_COMMAND_OSCAM += openssl dgst -sha256      -sign $(SIGN_PRIVKEY)      -out $(SIGN_DIGEST) $(SIGN_HASH);
-		SIGN_COMMAND_OSCAM += openssl dgst -sha256    -verify $(SIGN_PUBKEY) -signature $(SIGN_DIGEST) $(SIGN_HASH) | tr -d '\n';
+		SIGN_VER       = ${shell ($(SSL) version 2>/dev/null || echo "n.a.") | head -n 1 | awk -F'(' '{ print $$1 }' | xargs}
+		SIGN_INFO      = $(shell echo '|  Signing  : $(SIGN_VER)\n|             $(SIGN_PUBALGO), $(SIGN_PUBBIT), $(SIGN_SIGALGO),\n|             Valid $(SIGN_VALID), $(SIGN_SUBJECT)\n')
+		SIGN_INFO_TOOL = $(shell echo '|  SSL      = $(SSL)\n')
+		SIGN_COMMAND_OSCAM += sha256sum $@ | awk '{ print $$1 }' | tr -d '\n' > $(SIGN_HASH);
+		SIGN_COMMAND_OSCAM += printf 'SIGN	SHA256('; stat -c %s $(SIGN_HASH) | tr -d '\n'; printf '): '; cat $(SIGN_HASH); printf ' -> ';
+		SIGN_COMMAND_OSCAM += $(SSL) x509 -pubkey -noout -in $(SIGN_CERT)         -out $(SIGN_PUBKEY);
+		SIGN_COMMAND_OSCAM += $(SSL) dgst -sha256      -sign $(SIGN_PRIVKEY)      -out $(SIGN_DIGEST) $(SIGN_HASH);
+		SIGN_COMMAND_OSCAM += $(SSL) dgst -sha256    -verify $(SIGN_PUBKEY) -signature $(SIGN_DIGEST) $(SIGN_HASH) | tr -d '\n';
 		SIGN_COMMAND_OSCAM += printf '$(SIGN_MARKER)' | cat - $(SIGN_DIGEST) >> $@;
 		SIGN_COMMAND_OSCAM += printf ' <- DIGEST('; stat -c %s $(SIGN_DIGEST) | tr -d '\n'; printf ')\n';
 	endif
@@ -461,6 +464,7 @@ all:
 |  CC       = $(CC)\n\
 |  STRIP    = $(STRIP)\n\
 $(UPX_INFO_TOOL)\
+$(SIGN_INFO_TOOL)\
 | Settings:\n\
 |  CONF_DIR = $(CONF_DIR)\n\
 |  CC_OPTS  = $(strip $(CC_OPTS))\n\

config.sh
@@ -390,7 +390,7 @@ list_config() {
 	not_have_flag USE_LIBCRYPTO && enabled_any READER_CONAX READER_CRYPTOWORKS READER_NAGRA READER_NAGRA_MERLIN && echo "CONFIG_LIB_BIGNUM=y" || echo "# CONFIG_LIB_BIGNUM=n"
 	enabled READER_NAGRA_MERLIN && echo "CONFIG_LIB_MDC2=y" || echo "# CONFIG_LIB_MDC2=n"
 	enabled READER_NAGRA_MERLIN && echo "CONFIG_LIB_FAST_AES=y" || echo "# CONFIG_LIB_FAST_AES=n"
-	enabled READER_NAGRA_MERLIN && echo "CONFIG_LIB_SHA256=y" || echo "# CONFIG_LIB_SHA256=n"
+	enabled_any READER_NAGRA_MERLIN WITH_SIGNING && echo "CONFIG_LIB_SHA256=y" || echo "# CONFIG_LIB_SHA256=n"
 	enabled_any READER_NAGRA READER_NAGRA_MERLIN && echo "CONFIG_READER_NAGRA_COMMON=y" || echo "# CONFIG_READER_NAGRA_COMMON=n"
 }
 
@@ -402,6 +402,7 @@ make_config_c() {
 		echo "const char *config_mak = \"CFG: openssl not found in PATH!\";"
 	else
 		echo "// This file is generated by ./config.sh --objdir $OBJDIR --make-config.mak"
+		printf "const char *config_ssl = \"$($OPENSSL version | head -n 1 | awk -F'(' '{ print $1 }' | xargs)\";\n\n"
 		echo "const char *config_mak ="
 		printf "  \"\\\nCFG: strings FILE | sed -n 's/^CFG~//p' | openssl enc -d -base64 | gzip -d\\\n\"\n"
 		gzip -9 < $OBJDIR/config.mak | $OPENSSL enc -base64 | while read LINE
@@ -637,6 +638,7 @@ config_dialog() {
 				exit 0
 			;;
 		esac
+		cp -f $tempfileconfig $configfile
 	done
 }
 

oscam-signing.c
@@ -2,7 +2,7 @@
 
 #include "globals.h"
 #include "oscam-signing.h"
-#include "cscrypt/sha1.h"
+#include "cscrypt/sha256.h"
 #include "oscam-string.h"
 #include "oscam-time.h"
 #include "oscam-files.h"
@@ -281,14 +281,14 @@ int verifyBin(const char *binfile, EVP_PKEY *pubkey)
 	osi.sign_digest_size = 0;
 	osi.hash_digest_size = 0;
 	osi.hash_size = 0;
-	osi.hash_sha1 = NULL;
+	osi.hash_sha256 = NULL;
 	EVP_MD_CTX *mctx = NULL;
 	DIGEST sign = {NULL, 0};
 
 	// Get binfile hash digest and encrypted signature
 	DIGEST hash = hashBinary(binfile, &sign);
 
-	// hash sha1
+	// hash sha256
 	osi.sign_digest_size = sign.size;
 	if (hash.data != NULL)
 	{
@@ -296,9 +296,9 @@ int verifyBin(const char *binfile, EVP_PKEY *pubkey)
 		hex_encode(hash.data, shaVal, hash.size);
 		osi.hash_digest_size = hash.size;
 		osi.hash_size = cs_strlen(shaVal);
-		if (cs_malloc(&osi.hash_sha1, osi.hash_size + 1))
+		if (cs_malloc(&osi.hash_sha256, osi.hash_size + 1))
 		{
-			cs_strncpy(osi.hash_sha1, strtolower(shaVal), osi.hash_size + 1);
+			cs_strncpy(osi.hash_sha256, strtolower(shaVal), osi.hash_size + 1);
 		}
 		free(hash.data);
 
@@ -319,13 +319,13 @@ int verifyBin(const char *binfile, EVP_PKEY *pubkey)
 					cs_log("Error: EVP_VerifyInit() failed");
 				}
 
-				// Update verification with hash_sha1
+				// Update verification with hash_sha256
 				if (!EVP_VerifyUpdate(mctx, shaVal, cs_strlen(shaVal)))
 				{
 					cs_log("Error: EVP_VerifyUpdate() failed");
 				}
 
-				// Finalize verification hash_sha1 against signature and public key
+				// Finalize verification hash_sha256 against signature and public key
 				bResult = EVP_VerifyFinal(mctx, sign.data, sign.size, pubkey);
 				osi.is_verified = (bResult == 1 ? true : false);
 			}
@@ -344,7 +344,7 @@ DIGEST hashBinary(const char *binfile, DIGEST *sign)
 	size_t file_size = 0;
 	size_t offset = 0;
 	size_t signature_size = 0;
-	char *data = NULL, *ptr = NULL, *p = NULL;
+	unsigned char *data = NULL, *ptr = NULL, *p = NULL;
 
 	if (cs_malloc(&fi, sizeof(struct stat)))
 	{
@@ -381,17 +381,19 @@ DIGEST hashBinary(const char *binfile, DIGEST *sign)
 				offset = file_size;
 			}
 
-			// SHA1 hash of binary content without encrypted signature part
-			SHA_CTX ctx;
-			SHA1_Init(&ctx);
-			SHA1_Update(&ctx, data, offset);
+			// SHA256 hash of binary content without encrypted signature part
+			mbedtls_sha256_context ctx;
+			mbedtls_sha256_init(&ctx);
+			mbedtls_sha256_starts(&ctx, 0);
+			mbedtls_sha256_update(&ctx, data, offset);
 			munmap(data, file_size);
 			close(fd);
 
 			// Return calculated digest
-			arRetval.data = (unsigned char *)OPENSSL_malloc(SHA_DIGEST_LENGTH);
-			arRetval.size = SHA_DIGEST_LENGTH;
-			SHA1_Final(arRetval.data, &ctx);
+			arRetval.data = (unsigned char *)OPENSSL_malloc(SHA256_DIGEST_LENGTH);
+			arRetval.size = SHA256_DIGEST_LENGTH;
+			mbedtls_sha256_finish(&ctx, arRetval.data);
+			mbedtls_sha256_free(&ctx);
 		}
 	}
 

oscam-signing.h
@@ -34,7 +34,7 @@ struct o_sign_info
 	int		sign_digest_size;
 	int		hash_digest_size;
 	int		hash_size;
-	char	*hash_sha1;
+	char	*hash_sha256;
 };
 
 extern struct o_sign_info osi;

oscam.c
@@ -73,6 +73,7 @@ static void ssl_done(void) { }
 #include "oscam-signing.h"
 #endif
 
+extern char *config_ssl;
 extern char *config_mak;
 
 /*****************************************************************************
@@ -398,13 +399,14 @@ static void write_versionfile(bool use_stdout)
 #ifdef WITH_SIGNING
 	fprintf(fp, "\n");
 	fprintf(fp, "Signature:      %s\n", (osi.is_verified ? "Valid - successfully verified using built-in Public Key" : "Invalid - wrong signature or internal error occured!"));
-	fprintf(fp, "  SHA1:         %s\n", osi.hash_sha1);
+	fprintf(fp, "  Signer:       %s\n", config_ssl);
+	fprintf(fp, "  SHA256:       %s\n", osi.hash_sha256);
 	fprintf(fp, "Certificate:    %s %s Certificate\n", ((osi.cert_is_valid_self || osi.cert_is_valid_system) ? "Trusted" : "Untrusted"), (osi.cert_is_cacert ? "CA" : "self signed"));
 	fprintf(fp, "  Subject:      %s\n", osi.cert_subject);
 	fprintf(fp, "  Issuer:       %s\n", osi.cert_issuer);
 	fprintf(fp, "  Version:      %d\n", osi.cert_version);
 	fprintf(fp, "  Serial:       %s\n", osi.cert_serial);
-	fprintf(fp, "  Fingerpr.:    %s\n", osi.cert_fingerprint);
+	fprintf(fp, "  Fingerprint:  %s\n", osi.cert_fingerprint);
 	fprintf(fp, "  Valid from:   %s\n", osi.cert_valid_from);
 	fprintf(fp, "  Valid to:     %s\n", osi.cert_valid_to);
 	fprintf(fp, "  Status:       %s\n", (osi.cert_is_expired ? "Expired" : "Valid"));